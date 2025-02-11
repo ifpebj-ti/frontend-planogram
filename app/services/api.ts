ee8487d38f4f83ae45515df90d9afee50d47884f
Maria Gabrielly <104575546+mgGabrielly@users.noreply.github.com>
2025-02-11 16:04:10 -0300

    Update api.ts

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,5 +1,5 @@
 //const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
-const API_URL="http://144.22.161.124:8080";
+const API_URL="https://backplanograma.website";
 
 export const api = {
   async get<T>(endpoint: string): Promise<T> {
